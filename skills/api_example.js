6f64533cdadf2533a0ed3c6cf1326408c145f129
nihiven <j.behanna@gmail.com>
2019-02-08 16:01:11 -0500

    Revert "wah wahhh"
    This reverts commit d8bf787d72550812716899f6a9fb33874e180007.

--- skills/api_example.js
+++ skills/api_example.js
@@ -2,10 +2,23 @@
 2019-02-08: jb - the beginning
 */
 
+var wordfilter = require('wordfilter');
 var request = new XMLHttpRequest();
 
 module.exports = function(controller) {
   dataz = "nada";
+
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
+
+  request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+    dataz = data.title;
+  }
+
+  // Send request
+  request.send();
 
   controller.hears(['^man (.*)','^man'], 'direct_message,direct_mention', function(bot, message) {
     bot.replyWithTyping(message, 'Dataz: ' + dataz);
