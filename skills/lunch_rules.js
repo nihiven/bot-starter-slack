fa6a911b664a6c4d7f4f4616747a2b5fd76acfa9
nihiven <j.behanna@gmail.com>
2019-02-13 09:09:00 -0500

    Update lunch_rules.js

--- skills/lunch_rules.js
+++ skills/lunch_rules.js
@@ -14,87 +14,98 @@ var wordfilter = require('wordfilter');
 module.exports = function(controller) {
 
   controller.hears('lunch', 'direct_message', function(bot, message) {
-    bot.reply(message, {
-      attachments:[{
-        title: 'Let\'s Do Lunch',
-        callback_id: '123',
-        attachment_type: 'default',
-        actions: [
-          {
-            "name":"menu",
-            "text": "Who's going?",
-            "value": "list-show",
-            "type": "button",
-          },
-          {
-            "name":"menu",
-            "text": "Quick Pick",
-            "value": "rand-all",
-            "type": "button",
-          },
-          {
-            "name":"menu",
-            "text": "Edit Lists",
-            "value": "list-edit",
-            "type": "button",
-          },
-        ]
-      }]
-    });
+    showMenu(bot, message);
   });
 
   controller.on('interactive_message_callback', function(bot, message) {
 
-    if (message.actions[0].name === "who") {
+    if (message.actions[0].name == "who") {
       va = message.actions[0].value;
+      randomPlace(bot, message, va);
+    }
 
-      if (va === "everyone") {
-        bot.reply(message, "Five Guys");
+    if (message.actions[0].name == "menu") {
+      if (message.actions[0].value == "list-show") {
+        showList(box, message)
       }
-
-      if (va === "no-jb") {
-        bot.reply(message, "Greek");
-      }
-
-      if (va === "no-zack") {
-        bot.reply(message, "PIZZA");
-      }
-    } // if
-
-    if (message.actions[0].name === "menu") {
-      if (message.actions[0].value === "list-show")
-        bot.replyInteractive(message, {
-          text: '...',
-          attachments: [{
-            title: 'My buttons',
-            callback_id: '123',
-            attachment_type: 'default',
-            actions:
-            [
-              {
-                "text": "Everyone",
-                "name": "who",
-                "value": "everyone",
-                "type": "button",
-              },
-              {
-                "text": "No JB",
-                "name": "who",
-                "value": "no-jb",
-                "type": "button",
-              },
-              {
-                "text": "No Zack",
-                "name": "who",
-                "value": "no-zack",
-                "type": "button",
-              },
-            ]
-          }]
-        }); // reply
-      }
-    } // if
+    }
 
   });
 
 };
+
+function randomPlace(bot, message, list) {
+  if (list == "everyone") {
+    bot.reply(message, "Five Guys");
+  }
+
+  if (list == "no-jb") {
+    bot.reply(message, "Greek");
+  }
+
+  if (list == "no-zack") {
+    bot.reply(message, "PIZZA");
+  }
+}
+
+function showMenu(bot, message) {
+  bot.reply(message, {
+    attachments:[{
+      title: 'Let\'s Do Lunch',
+      callback_id: '123',
+      attachment_type: 'default',
+      actions: [
+        {
+          "name":"menu",
+          "text": "Who's going?",
+          "value": "list-show",
+          "type": "button",
+        },
+        {
+          "name":"menu",
+          "text": "Quick Pick",
+          "value": "rand-all",
+          "type": "button",
+        },
+        {
+          "name":"menu",
+          "text": "Edit Lists",
+          "value": "list-edit",
+          "type": "button",
+        },
+      ]
+    }]
+  });
+}
+
+function showLists(bot, message) {
+  bot.replyInteractive(message, {
+    text: '...',
+    attachments: [{
+      title: 'Who is going?',
+      callback_id: '123',
+      attachment_type: 'default',
+      actions:
+      [
+        {
+          "text": "Everyone",
+          "name": "who",
+          "value": "everyone",
+          "type": "button",
+        },
+        {
+          "text": "No JB",
+          "name": "who",
+          "value": "no-jb",
+          "type": "button",
+        },
+        {
+          "text": "No Zack",
+          "name": "who",
+          "value": "no-zack",
+          "type": "button",
+        },
+      ]
+    }]
+  }); // reply
+}
