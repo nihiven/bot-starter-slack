d61672747bfdd6c1ef9e2e96c94143f6c3a62e5d
nihiven <j.behanna@gmail.com>
2019-02-13 09:02:45 -0500

    Update lunch_rules.js

--- skills/lunch_rules.js
+++ skills/lunch_rules.js
@@ -21,19 +21,19 @@ module.exports = function(controller) {
         attachment_type: 'default',
         actions: [
           {
-            "name":"yes",
-            "text": "Random Place",
-            "value": "rand-show",
+            "name":"menu",
+            "text": "Who's going?",
+            "value": "list-show",
             "type": "button",
           },
           {
-            "name":"no",
+            "name":"menu",
             "text": "Quick Pick",
             "value": "rand-all",
             "type": "button",
           },
           {
-            "name":"no",
+            "name":"menu",
             "text": "Edit Lists",
             "value": "list-edit",
             "type": "button",
@@ -45,36 +45,56 @@ module.exports = function(controller) {
 
   controller.on('interactive_message_callback', function(bot, message) {
 
-    bot.reply(message, message.actions[0].name);
-    bot.replyInteractive(message, {
-      text: '...',
-      attachments: [{
-        title: 'My buttons',
-        callback_id: '123',
-        attachment_type: 'default',
-        actions:
-        [
-          {
-            "text": "Everyone",
-            "name": "who",
-            "value": "everyone",
-            "type": "button",
-          },
-          {
-            "text": "No JB",
-            "name": "who",
-            "value": "nojb",
-            "type": "button",
-          },
-          {
-            "text": "No Zack",
-            "name": "who",
-            "value": "nozack",
-            "type": "button",
-          },
-        ]
-      }]
-    });
+    if (message.actions[0].name === "who") {
+      va = message.actions[0].value;
+
+      if (va === "everyone") {
+        bot.reply(message, "Five Guys");
+      }
+
+      if (va === "no-jb") {
+        bot.reply(message, "Greek");
+      }
+
+      if (va === "no-zack") {
+        bot.reply(message, "PIZZA");
+      }
+    } // if
+
+    if (message.actions[0].name === "menu") {
+      if (message.actions[0].value === "list-show")
+        bot.replyInteractive(message, {
+          text: '...',
+          attachments: [{
+            title: 'My buttons',
+            callback_id: '123',
+            attachment_type: 'default',
+            actions:
+            [
+              {
+                "text": "Everyone",
+                "name": "who",
+                "value": "everyone",
+                "type": "button",
+              },
+              {
+                "text": "No JB",
+                "name": "who",
+                "value": "no-jb",
+                "type": "button",
+              },
+              {
+                "text": "No Zack",
+                "name": "who",
+                "value": "no-zack",
+                "type": "button",
+              },
+            ]
+          }]
+        }); // reply
+      }
+    } // if
+
   });
 
 };
